3e5894e7c4a972eb81dad0feadf8e15be40c19ef
epepftw <efgbt17@gmail.com>
2023-02-12 23:17:48 +0800

    Getting Random Numbers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
-import { trpc } from "@/utils/trpc";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
+// import { trpc } from "@/utils/trpc";
 
 export default function Home() {
-  const hello = trpc.hello.useQuery({ text: "bro" });
-  const name = trpc.deenice.useQuery({ owner: "Balakahye", age: 25 });
+  // const hello = trpc.hello.useQuery({ text: "bro" });
+  // const name = trpc.deenice.useQuery({ owner: "Balakahye", age: 25 });
 
-  if (!hello.data || !name.data) {
-    return <div>Loading...</div>;
-  }
+  // if (!hello.data || !name.data) {
+  //   return <div>Loading...</div>;
+  // }
+
+  const [first, second] = getOptionsForVote();
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-600">{first}</div>
         <div className="p-8">VS</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-600">{second}</div>
       </div>
     </div>
   );
