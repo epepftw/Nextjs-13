3d457b9049ff9649a3f81d0f6a83fd986cf09d7e
epepftw <efgbt17@gmail.com>
2023-02-12 22:37:37 +0800

    Basic UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,14 @@ export default function Home() {
     return <div>Loading...</div>;
   }
   return (
-    <div>
-      <p>{hello.data.greeting}</p>
-      <h1>{name.data.name}</h1>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
     </div>
   );
 }
